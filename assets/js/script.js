e26724a93a35e0ee64fe8804cf139420d54dd411
Jon Yeatman <jonlyeatman@gmail.com>
2021-02-20 23:25:08 -0500

    fluffing JS file and fixing timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,51 +24,37 @@ let questions = [
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+        "Document Oppressive Model",
+        "Document Object Model",
+        "Decisive Order Movement",
+        "Dictator Operations Model"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "What is JavaScript responsible for?",
+    answer: "Javascript programs the behavior and interactivity of the web page",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+        "JavaScript spcifies the design and layout of the web page",
+        "JavaScript defines the content of the web page",
+        "Javascript programs the behavior and interactivity of the web page",
+        "None of the above"
     ]
   },
     {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question: "What is Bootstrap?",
+    answer: "CSS Framework",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "CSS Framework",
+      "A way to secure your boots",
+      "JS Library",
+      "Visual Studio Plug-in"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
 
 
